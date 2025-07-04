62761b93cb723f91597f475687d4627598ed6c42
Francisco Junior <fgsj.dev@outlook.com>
2025-07-04 11:30:35 -0300

    First commit about level exercises

---
+++ Python Programming/beginner/v1.0/DocumentationPractice.py
@@ -0,0 +1,53 @@
+"""
+Milestone: development documentation with examples basics for learning language.
+Author: Francisco Gomes da Silva Junior
+Date: 04/07/2025 - format Brazil
+Version: 1.0
+"""
+
+# -------------------------------------------------------------------
+# Using function built-in called "print"
+# Level = Beginner
+# -------------------------------------------------------------------
+print("Hello World")
+
+# print with concatenation and variables
+number_one = 10
+number_two = 20
+sum_numbers = number_one + number_two
+print("The sum of two numbers is: ", sum_numbers)
+
+
+# Print with input data type "int" user and value final
+print("----------- Practice 01 -----------")
+variable_one = int(input("variable one: "))
+variable_two = int(input("variable two: "))
+sum_variables = variable_one + variable_two
+print("The value final is: ", sum_variables)
+
+# Print with input data type "String" user
+print("----------- Practice 02 -----------")
+first_name = str(input("First string: "))
+last_name = str(input("Last name: "))
+name_complete = first_name + last_name
+print("Final name is: %s" % name_complete)
+
+# Print with input data type "Float" user
+print("----------- Practice 03 -----------")
+number_floating = float(input("number one float: "))
+number_two_floating = float(input("number two float: "))
+print("The two numbers floating is: %f %f" % (number_floating, number_two_floating))
+
+# -------------------------------------------------------------------
+
+"""
+Exercises practice level of beginner
+Source: PYnative - https://pynative.com/python-basic-exercise-for-beginners/
+Date: 04/07/2025 - in format Brazil
+"""
+print("Exercise 01")
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+product_number = number_one * number_two
+
+# -------------------------------------------------------------------

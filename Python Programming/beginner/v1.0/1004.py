44ab52e0e3f950e57cb60b944ad7013d5108acb3
Francisco Junior <fgsj.dev@outlook.com>
2025-07-04 13:45:22 -0300

    sum of two numbers using methods interpolation

---
+++ Python Programming/beginner/v1.0/1004.py
@@ -0,0 +1,7 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+numbers_sum = number_one + number_two
+print("Sum of two numbers is: ", numbers_sum)
+
+# Printing direct in function "print" using interpolation
+print("Sum of %d + %d = %d" % (number_one, number_two, numbers_sum))

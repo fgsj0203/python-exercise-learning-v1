09838bd5df96773b93a61c096a34b73c4676b0e8
Francisco Junior <fgsj.dev@outlook.com>
2025-07-04 13:53:11 -0300

    calculate average of two numbers with weight

---
+++ Python Programming/beginner/v1.0/bee1005.py
@@ -0,0 +1,4 @@
+one_number = float(input("Number one: "))
+two_number = float(input("Number two: "))
+average_numbers = (one_number * 3.5 + two_number * 7.5) / (3.5 + 7.5)
+print("Average of two numbers is: %.5f" % average_numbers)

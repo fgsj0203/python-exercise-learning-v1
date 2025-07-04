ffae96a112af3d3f11dcf1dbcffd3f799b43b603
Francisco Junior <fgsj.dev@outlook.com>
2025-07-04 14:18:18 -0300

    calculate difference in four numbers

---
+++ Python Programming/beginner/v1.0/bee1007.py
@@ -0,0 +1,6 @@
+a = int(input("value A: "))
+b = int(input("value B: "))
+c = int(input("value C: "))
+d = int(input("value D: "))
+calculate_difference = a * b - c * d
+print("Difference is: %d" % calculate_difference)

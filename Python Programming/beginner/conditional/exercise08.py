0ba801f3d018aac7008cac52e5c937a77a8345f5
Francisco Junior <fgsj.dev@outlook.com>
2025-07-08 15:54:43 -0300

    Identifing day of week

---
+++ Python Programming/beginner/conditional/exercise08.py
@@ -0,0 +1,18 @@
+number_day = int(input("Input a number between 1-7\n"))
+
+print("Your print number: %d" % number_day)
+
+if number_day == 1:
+    print("domingo")
+elif number_day == 2:
+    print("segunda feira")
+elif number_day == 3:
+    print("terça feira")
+elif number_day == 4:
+    print("quarta feira")
+elif number_day == 5:
+    print("quinta feira")
+elif number_day == 6:
+    print("sexta feira")
+elif number_day == 7:
+    print("sábado")
